e6d9f1d8173f1a6c4f9ad0547175b2dfa54d79a4
朱晓江 <xiaojiang.zhu@mihoyo.com>
2022-02-28 19:12:01 +0800

    part

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,157 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Gantt :taskParams="taskParams" />
+    <div id="tt"></div>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+<script>
+// import { Component, Vue } from "vue-property-decorator";
+import Gg from "./components/gt/src";
+import Gantt from "./components/gantt/index.vue";
 
-@Component({
+export default {
   components: {
-    HelloWorld,
+    Gantt,
   },
-})
-export default class App extends Vue {}
+  data() {
+    return {
+      taskParams: [
+        {
+          start: "2014-10-03",
+          end: "2014-10-08",
+          name: "任务一",
+          id: "Task 0",
+          progress: 20,
+          handler: "张三",
+        },
+        {
+          start: "2014-10-03",
+          end: "2014-10-06",
+          name: "任务二",
+          id: "Task 1",
+          progress: 5,
+          dependencies: "Task 0",
+          handler: "张三",
+        },
+        {
+          start: "2014-10-04",
+          end: "2014-10-08",
+          name: "任务三",
+          id: "Task 2",
+          progress: 10,
+          dependencies: "Task 1",
+          handler: "张三",
+        },
+        {
+          start: "2014-10-08",
+          end: "2014-10-09",
+          name: "任务四",
+          id: "Task 3",
+          progress: 5,
+          dependencies: "Task 2",
+          handler: "张三",
+        },
+        {
+          start: "2014-10-08",
+          end: "2014-10-10",
+          name: "任务五",
+          id: "Task 4",
+          progress: 0,
+          dependencies: "Task 2",
+          handler: "张三",
+        },
+        {
+          start: "2014-10-11",
+          end: "2014-10-11",
+          name: "任务六",
+          id: "Task 5",
+          progress: 0,
+          dependencies: "Task 4",
+          custom_class: "bar-milestone",
+          handler: "张三",
+        },
+      ],
+    };
+  },
+  mounted() {
+    new Gg(
+      "#tt",
+      [
+        {
+          start: "2014-10-03",
+          end: "2014-10-08",
+          name: "任务一",
+          id: "Task 0",
+          progress: 20,
+          handler: "张三",
+        },
+        {
+          start: "2014-10-03",
+          end: "2014-10-06",
+          name: "任务二",
+          id: "Task 1",
+          progress: 5,
+          dependencies: "Task 0",
+          handler: "张三",
+        },
+        {
+          start: "2014-10-04",
+          end: "2014-10-08",
+          name: "任务三",
+          id: "Task 2",
+          progress: 10,
+          dependencies: "Task 1",
+          handler: "张三",
+        },
+        {
+          start: "2014-10-08",
+          end: "2014-10-09",
+          name: "任务四",
+          id: "Task 3",
+          progress: 5,
+          dependencies: "Task 2",
+          handler: "张三",
+        },
+        {
+          start: "2014-10-08",
+          end: "2014-10-10",
+          name: "任务五",
+          id: "Task 4",
+          progress: 0,
+          dependencies: "Task 2",
+          handler: "张三",
+        },
+        {
+          start: "2014-10-11",
+          end: "2014-10-11",
+          name: "任务六",
+          id: "Task 5",
+          progress: 0,
+          dependencies: "Task 4",
+          custom_class: "bar-milestone",
+          handler: "张三",
+        },
+      ],
+      {
+        on_click: function (task) {
+          console.log(task);
+        },
+        on_date_change: function (task, start, end) {
+          console.log(task, start, end);
+        },
+        on_progress_change: function (task, progress) {
+          console.log(task, progress);
+        },
+        on_view_change: function (mode) {
+          console.log(mode);
+        },
+        view_mode: "Day",
+        language: "zh",
+      }
+    );
+  },
+};
 </script>
 
 <style lang="scss">
